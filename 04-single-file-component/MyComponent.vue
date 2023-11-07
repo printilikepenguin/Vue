5b9ed7cda4477d9fe4eb17856792ccf8dee96a50
기면지 <hanguel97@gmail.com>
2023-11-07 17:58:36 +0900

    1107

---
+++ 04-single-file-component/MyComponent.vue
@@ -0,0 +1,21 @@
+<!-- vbase 탭, 셋업 -->
+
+<template>
+  <div class="greeting">
+    {{ msg }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('hello world!')
+</script>
+
+<!-- scoped는 템플릿에만 쓸 거라고 묶어주는 것 -->
+<!-- 스타일은 여러개가 될 수 있음 -->
+<style scoped>
+.greeting {
+  color: aquamarine;
+}
+</style>
